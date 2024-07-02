7635035ef617758070eb6a3e6ba5eefdd1eed45c
Mikssxed <eksilonxd@gmail.com>
2024-07-02 19:09:19 +0200

    convex setup, clerk setup, podcast page

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://credible-gecko-31.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
